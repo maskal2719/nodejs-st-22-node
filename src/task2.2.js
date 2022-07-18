3045137b42cdb535432ce40c2673aa6748367e09
Stanislav Moskalenko <maskal2719@gmail.com>
2022-07-18 10:29:45 +0300

    fix task 2.2

---
+++ src/task2.2.js
@@ -0,0 +1,14 @@
+const csv = require('csvtojson');
+const fs = require('fs')
+
+const csvFilePath = './csv/ex1.csv';
+const txtFilePath = 'result.txt';
+
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream(txtFilePath);
+
+readStream
+    .pipe(csv({
+        delimiter: ',',
+        colParser:{amount:'number', price: 'number'}, noheader: false, headers:['book','author','amount','price']}))
+    .pipe(writeStream);
