a66efbd51c74f916e53969e9c6e45a718cf581c7
Stanislav Moskalenko <maskal2719@gmail.com>
2022-07-18 10:17:15 +0300

    fix amount and price to number

--- task2.2.js
+++ task2.2.js
@@ -4,10 +4,14 @@ const fs = require('fs')
 const csvFilePath = './csv/ex1.csv';
 const txtFilePath = 'result.txt';
 
+const columnNumbers = ['amount', 'price'];
+
+
 const readStream = fs.createReadStream(csvFilePath);
 const writeStream = fs.createWriteStream(txtFilePath);
 
 readStream
-    .on('error', (error) => console.error(error))
-    .pipe(csv())
+    .pipe(csv({
+        delimiter: ',',
+        colParser:{amount:'number', price: 'number'}, noheader: false, headers:['book','author','amount','price']}))
     .pipe(writeStream);
