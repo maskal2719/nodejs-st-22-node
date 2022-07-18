2ecdd1c541f93d976a033dbc0eaadf6a3601ebf4
Stanislav Moskalenko <maskal2719@gmail.com>
2022-07-18 13:29:36 +0300

    fix task 2.3

--- src/task2.2.js
+++ src/task2.2.js
@@ -10,5 +10,10 @@ const writeStream = fs.createWriteStream(txtFilePath);
 readStream
     .pipe(csv({
         delimiter: ',',
+        ignoreColumns: /(amount)/,
         colParser:{amount:'number', price: 'number'}, noheader: false, headers:['book','author','amount','price']}))
     .pipe(writeStream);
+
+    readStream.on('error', error => {
+        console.log(error.message)
+    });
